28442be2f5bf766f21883234e1c4f25a3c786617
Fracisco Acosta <rwilly203@gmail.com>
2025-03-13 16:07:41 -0600

    new proyect
    starting the new proyect

---
+++ angularGpt/src/app/presentation/layouts/dashboardLayout/dashboardLayout.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-layout',
+  imports: [],
+  templateUrl: './dashboardLayout.component.html',
+  styleUrl: './dashboardLayout.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardLayoutComponent { }
