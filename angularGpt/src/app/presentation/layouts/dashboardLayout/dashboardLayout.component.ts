a6970f46f9a921d8799df547ffb790ea55586c54
Fracisco Acosta <rwilly203@gmail.com>
2025-03-19 17:59:37 -0600

    starting
    starting

--- angularGpt/src/app/presentation/layouts/dashboardLayout/dashboardLayout.component.ts
+++ angularGpt/src/app/presentation/layouts/dashboardLayout/dashboardLayout.component.ts
@@ -1,10 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
   templateUrl: './dashboardLayout.component.html',
-  styleUrl: './dashboardLayout.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardLayoutComponent { }
+
